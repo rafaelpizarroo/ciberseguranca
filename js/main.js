14d1bb5782624a612b0947214786ecf6c684ef7c
Rafael Pizarro <207749@upf.br>
2025-06-26 20:11:27 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 let corpo = document.body;
 let navegacao = document.getElementById('navegacao');
+let estudantes = document.getElementById('estudantes')
 
 document.getElementById('TemaClaroEscuroBtn').addEventListener('click', () => {
     corpo.classList.toggle('white')
     corpo.classList.toggle('dark')
     navegacao.classList.toggle('navwhite')
     navegacao.classList.toggle('navdark')
+    estudantes.classList.toggle('white')
+    estudantes.classList.toggle('dark')
 })
 
 document.getElementById('acessibilidadeBtn').addEventListener('click', () => {
     corpo.classList.toggle('acessibilidade');
-});
+}); 
